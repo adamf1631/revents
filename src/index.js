1963ed1c3e09995634a5b166aa9d9386854c1fae
adamf1631 <adamf1631@yahoo.com>
2022-12-12 20:44:26 -0500

    Section 1

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app/layout/App";
+
+const rootEl = document.getElementById("root");
+
+function render() {
+  ReactDOM.render(<App />, rootEl);
+}
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", function () {
+    setTimeout(render);
+  });
+}
+
+render();
